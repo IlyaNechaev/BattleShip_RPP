7a82d719986c469db0ac50a9e145a8c86ae9cb66
Bergolfs <46860574+Bergolfs@users.noreply.github.com>
2019-03-14 10:04:01 +0300

    Add files via upload

---
+++ Mine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Mine : IShip
+    {
+        ICell _cell;
+        public int Size { get; }
+        public bool IsKilled { get; }
+        public bool NeedToBeKilled { get; }
+
+        public Mine()
+        {
+            Size = 1;
+            IsKilled = false;
+            NeedToBeKilled = false;
+        }
+
+        public ICell GetCell(int index)
+        {
+            return _cell;
+        }
+    }
+}
---
+++ Ship.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Ship : IShip
+    {
+        private ICell[] _cells;
+        public int Size { get; }
+        public bool IsKilled { get; }
+        public bool NeedToBeKilled { get; }
+
+        public Ship(int size, ICell[] cells)
+        {
+            if (size > 4 || size < 1)
+                ;
+            Size = size;
+            IsKilled = false;
+            NeedToBeKilled = true;
+            _cells = new ICell[size];
+            cells.CopyTo(_cells, 0);
+        }
+
+        public ICell GetCell(int index)
+        {
+            return _cells[index];
+        }
+
+        public GetHit()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                if 
+            }
+        }
+    }
+}
